d776cfb3de9cc4947513b2f2a6ed925f73b18213
Abdelrahman Omran <me@omranic.com>
2017-03-11 15:01:01 +0200

    Turn off nginx server_tokens

--- provision.sh
+++ provision.sh
@@ -54,6 +54,9 @@ sudo su rinvex <<'EOF'
 /usr/local/bin/composer global require "laravel/installer=~1.1"
 EOF
 
+# Hide nginx server version
+ sed -i "s/# server_tokens off;/server_tokens off;/" /etc/nginx/nginx.conf
+ 
 # Remove default nginx host
 rm -rvf /etc/nginx/sites-enabled/default
 rm -rvf /etc/nginx/sites-available/default
@@ -64,7 +67,7 @@ sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.1/cli/php.ini
 sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.1/cli/php.ini
 sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.1/cli/php.ini
 
-# Setup Some PHP-FPM Options
+# Set Some PHP-FPM Options
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.1/fpm/php.ini
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.1/fpm/php.ini
 sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.1/fpm/php.ini
