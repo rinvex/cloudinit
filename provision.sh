4d14053e7b157ac9d658cdaae7e5532d1efb90aa
Abdelrahman Omran <me@omranic.com>
2017-03-12 08:20:54 +0200

    Revert "Install gnupg2 package"
    This reverts commit c676d1b2d51ed54caaa15b0ea97d90c08f9e1416.

--- provision.sh
+++ provision.sh
@@ -32,7 +32,7 @@ apt-get update
 # Install Some Basic Packages
 apt-get install -y --allow-unauthenticated \
 build-essential gcc libmcrypt4 libpcre3-dev ntp unzip \
-make python2.7-dev python-pip whois gnupg2 \
+make python2.7-dev python-pip whois letsencrypt \
 php7.1-cli php7.1-dev \
 php7.1-pgsql php7.1-sqlite3 php7.1-gd \
 php7.1-curl php7.1-memcached \
