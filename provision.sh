44b9bbc5c6e683e9f3db82380b11f5ae04053b0d
Abdelrahman Omran <me@omranic.com>
2017-03-17 20:22:42 +0200

    Fix missing space between package names installation command

--- provision.sh
+++ provision.sh
@@ -34,8 +34,8 @@ apt-get update
 
 # Install Some Basic Packages
 apt-get install -y build-essential libpcre3-dev python2.7-dev ntp \
-python-pip whois nginx sqlite3 nodejs wkhtmltopdf gcc libmcrypt4 unzip make\
-php7.1-cli php7.1-dev php7.1-pgsql php7.1-sqlite3 php7.1-gd php7.1-fpm php7.1-xml\
+python-pip whois nginx sqlite3 nodejs wkhtmltopdf gcc libmcrypt4 unzip make \
+php7.1-cli php7.1-dev php7.1-pgsql php7.1-sqlite3 php7.1-gd php7.1-fpm php7.1-xml \
 php7.1-curl php7.1-memcached php7.1-imap php7.1-mysql php7.1-mbstring \
 php7.1-zip php7.1-bcmath php7.1-soap php7.1-intl php7.1-readline \
 
