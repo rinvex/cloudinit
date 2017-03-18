1ba35494d76a4e7bdb9a4a20850abea6bb385009
Abdelrahman Omran <me@omranic.com>
2017-03-18 16:48:44 +0200

    Update nginx SSL config

--- provision.sh
+++ provision.sh
@@ -119,10 +119,47 @@ sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.1/fpm/pool.d/www.conf
 # Generate Strong Diffie-Hellman Group
 openssl dhparam -out /etc/nginx/dhparam.pem 2048
 
-# Create global nginx hooks folders
-mkdir /etc/nginx/rinvex-conf/global/before -p 2>/dev/null
-mkdir /etc/nginx/rinvex-conf/global/server -p 2>/dev/null
-mkdir /etc/nginx/rinvex-conf/global/after -p 2>/dev/null
+# Create global nginx snippets folders
+mkdir /etc/nginx/snippets/global/before -p 2>/dev/null
+mkdir /etc/nginx/snippets/global/server -p 2>/dev/null
+mkdir /etc/nginx/snippets/global/general -p 2>/dev/null
+mkdir /etc/nginx/snippets/global/after -p 2>/dev/null
+
+# Write server headers
+headers="# HSTS and other security headers (ngx_http_headers_module is required) (15768000 seconds = 6 months)
+add_header Strict-Transport-Security max-age=15768000;
+add_header X-Frame-Options \"SAMEORIGIN\";
+add_header X-XSS-Protection \"1; mode=block\";
+add_header X-Content-Type-Options \"nosniff\";
+"
+
+echo "\$headers" > "/etc/nginx/snippets/global/general/headers.conf"
+
+
+# Write SSL flags
+ssl="
+    # Based on Mozilla SSL Configuration Generator
+    # https://mozilla.github.io/server-side-tls/ssl-config-generator/
+    ssl_session_timeout 1d;
+    ssl_session_cache shared:SSL:50m;
+    ssl_session_tickets off;
+
+    # Diffie-Hellman parameter for DHE ciphersuites, recommended 2048 bits
+    ssl_dhparam /etc/nginx/dhparam.pem;
+
+    # intermediate configuration. tweak to your needs.
+    ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+    ssl_ciphers 'ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS';
+    ssl_prefer_server_ciphers on;
+
+    # OCSP Stapling ---
+    # fetch OCSP records from URL in ssl_certificate and cache them
+    ssl_stapling on;
+    ssl_stapling_verify on;
+"
+
+echo "\$ssl" > "/etc/nginx/snippets/global/general/ssl.conf"
+
 
 # Add letsencrypt cronjob
 sudo su rinvex <<'EOF'
