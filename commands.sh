dd6af003145b00b915a32367f1506220a61b9dec
Abdelrahman Omran <me@omranic.com>
2017-03-18 16:45:59 +0200

    Update nginx SSL config

--- commands.sh
+++ commands.sh
@@ -30,10 +30,24 @@ include snippets/global/before/*;
 include snippets/\$1/before/*;
 
 server {
-    listen \${4:-80};
     listen \${5:-443} ssl http2;
     server_name \$1;
     root \"\$2\";
+
+    # certs sent to the client in SERVER HELLO are concatenated in ssl_certificate
+    ssl_certificate /etc/letsencrypt/live/\$1/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/\$1/privkey.pem;
+
+    # NGINX SNIPPETS (DOT NOT REMOVE!)
+    include snippets/global/general/ssl.conf;
+    include snippets/global/general/headers.conf;
+
+    ## verify chain of trust of OCSP response using Root CA and Intermediate certs
+    ssl_trusted_certificate /etc/letsencrypt/live/\$1/fullchain.pem;
+
+    # Use Google DNS servers for upstream dns resolving
+    resolver 8.8.8.8 8.8.4.4 valid=300s;
+    resolver_timeout 5s;
 
     index index.html index.htm index.php;
 
@@ -94,30 +108,16 @@ server {
 
 # Redirect SSL to primary domain SSL...
 server {
-    # Based on Mozilla SSL Configuration Generator
-    # https://mozilla.github.io/server-side-tls/ssl-config-generator/
     listen \${5:-443} ssl http2;
     listen [::]:\${5:-443} ssl http2;
 
     # certs sent to the client in SERVER HELLO are concatenated in ssl_certificate
     ssl_certificate /etc/letsencrypt/live/\$1/fullchain.pem;
     ssl_certificate_key /etc/letsencrypt/live/\$1/privkey.pem;
-    ssl_session_timeout 1d;
-    ssl_session_cache shared:SSL:50m;
-    ssl_session_tickets off;
 
-    # Diffie-Hellman parameter for DHE ciphersuites, recommended 2048 bits
-    ssl_dhparam /etc/nginx/dhparam.pem;
-
-    # intermediate configuration. tweak to your needs.
-    ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
-    ssl_ciphers 'ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS';
-    ssl_prefer_server_ciphers on;
-
-    # OCSP Stapling ---
-    # fetch OCSP records from URL in ssl_certificate and cache them
-    ssl_stapling on;
-    ssl_stapling_verify on;
+    # NGINX SNIPPETS (DOT NOT REMOVE!)
+    include snippets/global/general/ssl.conf;
+    include snippets/global/general/headers.conf;
 
     ## verify chain of trust of OCSP response using Root CA and Intermediate certs
     ssl_trusted_certificate /etc/letsencrypt/live/\$1/fullchain.pem;
@@ -132,16 +132,6 @@ server {
 "
 
 echo "\$ssl_redirect" > "/etc/nginx/snippets/\$1/before/ssl_redirect.conf"
-
-# Write server headers
-server_headers="# HSTS and other security headers (ngx_http_headers_module is required) (15768000 seconds = 6 months)
-add_header Strict-Transport-Security max-age=15768000;
-add_header X-Frame-Options \"SAMEORIGIN\";
-add_header X-XSS-Protection \"1; mode=block\";
-add_header X-Content-Type-Options \"nosniff\";
-"
-
-echo "\$server_headers" > "/etc/nginx/snippets/\$1/server/server_headers.conf"
 
 # Start nginx service
 /etc/init.d/nginx start
