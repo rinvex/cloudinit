25112b3e002c83eefc9205a6aff9bab7e62a5156
Abdelrahman Omran <me@omranic.com>
2017-06-24 05:45:29 +0200

    Fix wrong composer self-update cronjob

--- provision.sh
+++ provision.sh
@@ -140,7 +140,7 @@ gssl $(curl http://169.254.169.254/latest/meta-data/public-ipv4 -s) default
 # Add letsencrypt renewal and composer self-update cronjobs
 sudo su <<'EOF'
 crontab -l | { cat; echo "0 */12 * * * certbot-auto renew --pre-hook \"sudo /etc/init.d/nginx stop\" --post-hook \"sudo /etc/init.d/nginx start\" --agree-tos --quiet"; } | crontab -
-crontab -l | { cat; echo "0 0 * * * root /usr/local/bin/composer self-update >> /var/log/composer.log 2>&1"; } | crontab -
+crontab -l | { cat; echo "0 0 * * * /usr/local/bin/composer self-update >> /var/log/composer.log 2>&1"; } | crontab -
 EOF
 
 # Add toran proxy cronjob as rinvex user
