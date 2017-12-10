e2a375dcfef375851f90172862778cccddb0cf69
Abdelrahman Omran <me@omranic.com>
2017-12-10 03:08:57 +0200

    Clean scripts for servers behind elastic load balancer

--- commands.sh
+++ commands.sh
@@ -10,7 +10,7 @@ cat > /usr/local/bin/serve << EOF
 set -e
 
 if [[ \$EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
+   echo "This script must be run as root"
    exit 1
 fi
 
@@ -24,113 +24,67 @@ echo "Creating nginx configuration files..."
 # Create nginx site log directory
 mkdir /var/log/nginx/\$1 -p 2>/dev/null
 
-server="# Redirect every HTTP request to non-www HTTPS...
-# Choose between www and non-www, listen on the *wrong* one and redirect to
-# the right one -- http://wiki.nginx.org/Pitfalls#Server_Name
+server="# Redirect all www. client requests non-www.
 server {
-  listen [::]:80;
-  listen 80;
-
-  # listen on both hosts
-  server_name \$1 www.\$1;
-
-  # Include nginx security headers
-  include /etc/nginx/snippets/headers.conf;
-
-  # and redirect to the https host (declared below)
-  # avoiding http://www -> https://www -> https:// chain.
-  return 301 https://\$1\\\$request_uri;
+    listen 80;
+    listen [::]:80;
+    server_name www.\$1;
+    return 301 https://\\\$host\\\$request_uri;
 }
 
 server {
-  listen [::]:443 ssl http2;
-  listen 443 ssl http2;
+    listen 80;
+    listen [::]:80;
 
-  # listen on the wrong host
-  server_name www.\$1;
+    server_name \$1;
+    root "\$2";
 
-  # Include nginx ssl config
-  include /etc/nginx/snippets/ssl.conf;
-  include /etc/nginx/snippets/headers.conf;
-  include /etc/nginx/snippets/ssl-stapling.conf;
+    # Redirect HTTP client requests to HTTPS
+    if (\\\$http_x_forwarded_proto != "https") {
+        return 301 https://\\\$host\\\$request_uri;
+    }
 
-  # certs sent to the client in SERVER HELLO are concatenated in ssl_certificate
-  ssl_certificate /etc/letsencrypt/live/\$1/fullchain.pem;
-  ssl_certificate_key /etc/letsencrypt/live/\$1/privkey.pem;
+    # Custom 404 page
+    error_page 404 /index.php;
 
-  ## verify chain of trust of OCSP response using Root CA and Intermediate certs
-  ssl_trusted_certificate /etc/letsencrypt/live/\$1/fullchain.pem;
+    # Include basic nginx server config
+    include /etc/nginx/snippets/headers.conf;
+    include /etc/nginx/snippets/expires.conf;
+    include /etc/nginx/snippets/cross-domain-fonts.conf;
+    include /etc/nginx/snippets/protect-system-files.conf;
 
-  # and redirect to the non-www host (declared below)
-  return 301 https://\$1\\\$request_uri;
-}
+    index index.html index.htm index.php;
 
-server {
-  listen [::]:443 ssl http2;
-  listen 443 ssl http2;
+    charset utf-8;
 
-  # The host name to respond to
-  server_name \$1;
+    location / {
+        try_files \\\$uri \\\$uri/ /index.php?\\\$query_string;
+    }
 
-  # Include nginx ssl config
-  include /etc/nginx/snippets/ssl.conf;
-  include /etc/nginx/snippets/ssl-stapling.conf;
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location = /robots.txt  { access_log off; log_not_found off; }
 
-  # certs sent to the client in SERVER HELLO are concatenated in ssl_certificate
-  ssl_certificate /etc/letsencrypt/live/\$1/fullchain.pem;
-  ssl_certificate_key /etc/letsencrypt/live/\$1/privkey.pem;
+    access_log  /var/log/nginx/\$1/access.log;
+    error_log  /var/log/nginx/\$1/error.log error;
 
-  ## verify chain of trust of OCSP response using Root CA and Intermediate certs
-  ssl_trusted_certificate /etc/letsencrypt/live/\$1/fullchain.pem;
+    location ~ \.php\$ {
+        fastcgi_split_path_info ^(.+\.php)(/.+)\$;
+        fastcgi_pass unix:/var/run/php/php7.1-fpm.sock;
+        fastcgi_index index.php;
+        include /etc/nginx/snippets/fastcgi_params.conf;
+    }
 
-  # Path for static files
-  root \$2;
-
-  # Specify a charset
-  charset utf-8;
-
-  # Custom 404 page
-  error_page 404 /index.php;
-
-  # Include basic nginx server config
-  include /etc/nginx/snippets/headers.conf;
-  include /etc/nginx/snippets/expires.conf;
-  include /etc/nginx/snippets/cross-domain-fonts.conf;
-  include /etc/nginx/snippets/protect-system-files.conf;
-
-  index index.html index.htm index.php;
-
-  location / {
-      try_files \\\$uri \\\$uri/ /index.php?\\\$query_string;
-  }
-
-  location = /favicon.ico { access_log off; log_not_found off; }
-  location = /robots.txt  { access_log off; log_not_found off; }
-
-  access_log /var/log/nginx/\$1/access.log main;
-  error_log /var/log/nginx/\$1/error.log warn;
-
-  location ~ \.php\$ {
-      fastcgi_split_path_info ^(.+\.php)(/.+)\$;
-      fastcgi_pass unix:/var/run/php/php7.1-fpm.sock;
-      fastcgi_index index.php;
-      include fastcgi_params;
-  }
+    location ~ /\.ht {
+        deny all;
+    }
 }
 "
 
 echo "\$server" > "/etc/nginx/sites-available/\$1"
 ln -fs "/etc/nginx/sites-available/\$1" "/etc/nginx/sites-enabled/\$1"
 
-# Stop nginx service
-/etc/init.d/nginx stop
-
-# Generate a new letsencrypt certificate
-echo "Generating letsencrypt certificate..."
-certbot-auto certonly --standalone --webroot-path \$2 --domain \$1 --domain www.\$1 --email \${3:-help@rinvex.com} --agree-tos
-
 # Start nginx service
-/etc/init.d/nginx start
+/etc/init.d/nginx restart
 
 echo "Done!"
 EOF
@@ -146,7 +100,7 @@ cat > /usr/local/bin/unserve << EOF
 set -e
 
 if [[ \$EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
+   echo "This script must be run as root"
    exit 1
 fi
 
@@ -154,11 +108,6 @@ if [[ \$# -eq 0 ]] || [[ -z "\$1" ]]; then
     echo "Invalid arguments provided! Usage: serve domain.ext"
     exit 1
 fi
-
-echo "Removing letsencrypt..."
-rm -rvf /etc/letsencrypt/live/\$1
-rm -rvf /etc/letsencrypt/archive/\$1
-rm -rvf /etc/letsencrypt/renewal/\$1.conf
 
 echo "Removing virtual host..."
 rm -rvf /etc/nginx/sites-enabled/\$1
@@ -206,7 +155,7 @@ if [[ -f artisan ]]; then
     php artisan route:clear
     php artisan config:clear
     php artisan migrate --force
-    
+
     if [[ \$2 -eq 'master' ]]; then
         php artisan route:cache
         php artisan config:cache
@@ -230,7 +179,7 @@ cat > /usr/local/bin/secure << EOF
 set -e
 
 if [[ \$EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
+   echo "This script must be run as root"
    exit 1
 fi
 
@@ -252,45 +201,3 @@ echo '-------------------------'
 EOF
 
 chmod +x /usr/local/bin/secure
-
-
-# Write gssl script
-# Usage: gssl domain.ext frinedlyName
-cat > /usr/local/bin/gssl << EOF
-#!/usr/bin/env bash
-
-set -e
-
-if [[ \$EUID -ne 0 ]]; then
-   echo "This script must be run as root"
-   exit 1
-fi
-
-if [[ \$# -eq 0 ]] || [[ -z "\$1" ]] || [[ -z "\$2" ]]; then
-    echo "Invalid arguments provided! Usage: gssl domain.ext frinedlyName"
-    exit 1
-fi
-
-# Set our CSR variables
-SUBJ="
-C=US
-ST=California
-O=
-localityName=San Francisco
-commonName=\$1
-organizationalUnitName=
-emailAddress=
-"
-
-# Create our SSL directory
-# in case it doesn't exist
-sudo mkdir -p "/etc/ssl/\$2"
-
-# Generate our Private Key, CSR and Certificate
-sudo openssl genrsa -out "/etc/ssl/\$2/\$2.key" 2048
-sudo openssl req -new -subj "\$(echo -n "\$SUBJ" | tr "\n" "/")" -key "/etc/ssl/\$2/\$2.key" -out "/etc/ssl/\$2/\$2.csr" -passin pass:""
-sudo openssl x509 -req -days 365 -in "/etc/ssl/\$2/\$2.csr" -signkey "/etc/ssl/\$2/\$2.key" -out "/etc/ssl/\$2/\$2.crt"
-
-EOF
-
-chmod +x /usr/local/bin/gssl
--- provision.sh
+++ provision.sh
@@ -27,7 +27,7 @@ sed -i "s/PermitRootLogin .*/PermitRootLogin no/" /etc/ssh/sshd_config
 # Install Some PPAs
 apt-add-repository ppa:ondrej/php -y
 apt-add-repository ppa:nginx/stable -y
-curl --silent --location https://deb.nodesource.com/setup_7.x | bash -
+curl --silent --location https://deb.nodesource.com/setup_9.x | sudo -E bash -
 
 # Update Package Lists
 apt-get update
@@ -38,12 +38,6 @@ python-pip whois nginx sqlite3 nodejs wkhtmltopdf gcc libmcrypt4 unzip make \
 php7.1-cli php7.1-dev php7.1-pgsql php7.1-sqlite3 php7.1-gd php7.1-fpm php7.1-xml \
 php7.1-curl php7.1-memcached php7.1-imap php7.1-mysql php7.1-mbstring \
 php7.1-zip php7.1-bcmath php7.1-soap php7.1-intl php7.1-readline \
-
-# Install certbot
-echo "Installing Certbot..."
-wget https://raw.githubusercontent.com/rinvex/cloudinit/master/certbot-auto -O /usr/local/bin/certbot-auto
-chmod a+x /usr/local/bin/certbot-auto
-certbot-auto --os-packages-only --quiet
 
 # Install Composer
 curl -sS https://getcomposer.org/installer | HOME="/home/rinvex" php -- --install-dir=/usr/local/bin --filename=composer
@@ -58,7 +52,7 @@ sudo su rinvex <<'EOF'
 /usr/local/bin/composer global require "laravel/envoy=~1.0"
 /usr/local/bin/composer global require "laravel/installer=~1.1"
 EOF
- 
+
 # Remove default nginx host
 rm -rvf /etc/nginx/sites-enabled/default
 rm -rvf /etc/nginx/sites-available/default
@@ -76,49 +70,23 @@ sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.1/fpm/php.ini
 sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.1/fpm/php.ini
 sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.1/fpm/php.ini
 
-# Copy fastcgi_params to Nginx because they broke it on the PPA
-cat > /etc/nginx/fastcgi_params << EOF
-fastcgi_param    QUERY_STRING        \$query_string;
-fastcgi_param    REQUEST_METHOD      \$request_method;
-fastcgi_param    CONTENT_TYPE        \$content_type;
-fastcgi_param    CONTENT_LENGTH      \$content_length;
-fastcgi_param    SCRIPT_FILENAME     \$request_filename;
-fastcgi_param    SCRIPT_NAME         \$fastcgi_script_name;
-fastcgi_param    REQUEST_URI         \$request_uri;
-fastcgi_param    DOCUMENT_URI        \$document_uri;
-fastcgi_param    DOCUMENT_ROOT       \$document_root;
-fastcgi_param    SERVER_PROTOCOL     \$server_protocol;
-fastcgi_param    GATEWAY_INTERFACE   CGI/1.1;
-fastcgi_param    SERVER_SOFTWARE     nginx/\$nginx_version;
-fastcgi_param    REMOTE_ADDR         \$remote_addr;
-fastcgi_param    REMOTE_PORT         \$remote_port;
-fastcgi_param    SERVER_ADDR         \$server_addr;
-fastcgi_param    SERVER_PORT         \$server_port;
-fastcgi_param    SERVER_NAME         \$server_name;
-fastcgi_param    HTTPS               \$https if_not_empty;
-fastcgi_param    REDIRECT_STATUS     200;
-EOF
-
 # Replace default nginx config with optimized one
 rm -rvf /etc/nginx/nginx.conf
 wget https://raw.githubusercontent.com/rinvex/cloudinit/master/nginx/nginx.conf -O /etc/nginx/nginx.conf
 
 # Download nginx snippets
-wget https://raw.githubusercontent.com/rinvex/cloudinit/master/nginx/snippets/ssl.conf -O /etc/nginx/snippets/ssl.conf
 wget https://raw.githubusercontent.com/rinvex/cloudinit/master/nginx/snippets/headers.conf -O /etc/nginx/snippets/headers.conf
 wget https://raw.githubusercontent.com/rinvex/cloudinit/master/nginx/snippets/expires.conf -O /etc/nginx/snippets/expires.conf
-wget https://raw.githubusercontent.com/rinvex/cloudinit/master/nginx/snippets/ssl-stapling.conf -O /etc/nginx/snippets/ssl-stapling.conf
+wget https://raw.githubusercontent.com/rinvex/cloudinit/master/nginx/snippets/fastcgi_params.conf -O /etc/nginx/snippets/fastcgi_params.conf
 wget https://raw.githubusercontent.com/rinvex/cloudinit/master/nginx/snippets/cross-domain-fonts.conf -O /etc/nginx/snippets/cross-domain-fonts.conf
 wget https://raw.githubusercontent.com/rinvex/cloudinit/master/nginx/snippets/protect-system-files.conf -O /etc/nginx/snippets/protect-system-files.conf
 wget https://raw.githubusercontent.com/rinvex/cloudinit/master/nginx/snippets/cross-domain-insecure.conf -O /etc/nginx/snippets/cross-domain-insecure.conf
 
 # Download nginx default sites
 wget https://raw.githubusercontent.com/rinvex/cloudinit/master/nginx/sites-available/no-default.conf -O /etc/nginx/sites-available/no-default.conf
-wget https://raw.githubusercontent.com/rinvex/cloudinit/master/nginx/sites-available/ssl.no-default.conf -O /etc/nginx/sites-available/ssl.no-default.conf
 
 # Enable default nginx sites
 ln -fs "/etc/nginx/sites-available/no-default.conf" "/etc/nginx/sites-enabled/no-default.conf"
-ln -fs "/etc/nginx/sites-available/ssl.no-default.conf" "/etc/nginx/sites-enabled/ssl.no-default.conf"
 
 # Set The PHP-FPM User
 sed -i "s/user = www-data/user = rinvex/" /etc/php/7.1/fpm/pool.d/www.conf
@@ -128,34 +96,14 @@ sed -i "s/listen\.owner.*/listen.owner = rinvex/" /etc/php/7.1/fpm/pool.d/www.co
 sed -i "s/listen\.group.*/listen.group = rinvex/" /etc/php/7.1/fpm/pool.d/www.conf
 sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.1/fpm/pool.d/www.conf
 
-# Generate Strong Diffie-Hellman Group
-openssl dhparam -out /etc/nginx/dhparam.pem 2048
-
-# Generate default ssl certificate
-gssl $(curl http://169.254.169.254/latest/meta-data/public-ipv4 -s) default
-
-# Restart nginx service
-/etc/init.d/nginx restart
-
-# Add letsencrypt renewal and composer self-update cronjobs
-sudo su <<'EOF'
-crontab -l | { cat; echo "0 */12 * * * certbot-auto renew --pre-hook \"sudo /etc/init.d/nginx stop\" --post-hook \"sudo /etc/init.d/nginx start\" --agree-tos --quiet"; } | crontab -
-crontab -l | { cat; echo "0 0 * * * /usr/local/bin/composer self-update >> /var/log/composer.log 2>&1"; } | crontab -
-EOF
-
-# Add toran proxy cronjob as rinvex user
-# sudo su rinvex <<'EOF'
-# crontab -l | { cat; echo "0 * * * * cd /home/rinvex/toran.domain.com && php bin/cron >> bin/cron.log"; } | crontab -
-# EOF
-
-# Restart nginx and php7.1-fpm services
-/etc/init.d/nginx restart
-/etc/init.d/php7.1-fpm restart
-
 # Add User To WWW-Data
 usermod -a -G www-data rinvex
 id rinvex
 groups rinvex
+
+# Restart nginx and php7.1-fpm services
+/etc/init.d/nginx restart
+/etc/init.d/php7.1-fpm restart
 
 # Clean Up
 apt-get -y autoremove
