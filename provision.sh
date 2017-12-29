c69f3e4f42f950bd98a5a44f79079dd143eaf9bd
Abdelrahman Omran <me@omranic.com>
2017-12-29 10:37:30 +0200

    Install svgo npm package (close #1)

--- provision.sh
+++ provision.sh
@@ -39,6 +39,9 @@ php7.2-cli php7.2-dev php7.2-pgsql php7.2-sqlite3 php7.2-gd php7.2-fpm php7.2-xm
 php7.2-curl php7.2-memcached php7.2-imap php7.2-mysql php7.2-mbstring \
 php7.2-zip php7.2-bcmath php7.2-soap php7.2-intl php7.2-readline \
 
+# Install svgo npm package
+sudo npm install -g svgo
+
 # Install Composer
 curl -sS https://getcomposer.org/installer | HOME="/home/coworkit" php -- --install-dir=/usr/local/bin --filename=composer
 chown coworkit:coworkit /home/coworkit/.composer -R
