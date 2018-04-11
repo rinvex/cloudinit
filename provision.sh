784807440de763a1af0fec31b669172925f06982
Abdelrahman Omran <me@omranic.com>
2018-04-11 11:13:23 +0200

    Install mysql-server and unattended-upgrades

--- provision.sh
+++ provision.sh
@@ -38,9 +38,10 @@ nginx sqlite3 nodejs wkhtmltopdf libmcrypt4 unzip make jpegoptim optipng pngquan
 php7.2-cli php7.2-dev php7.2-pgsql php7.2-sqlite3 php7.2-gd php7.2-fpm php7.2-xml \
 php7.2-curl php7.2-memcached php7.2-imap php7.2-mysql php7.2-mbstring \
 php7.2-zip php7.2-bcmath php7.2-soap php7.2-intl php7.2-readline \
+mysql-server unattended-upgrades
 
 # Install svgo npm package
-sudo npm install -g svgo
+npm install -g svgo
 
 # Install Composer
 curl -sS https://getcomposer.org/installer | HOME="/home/coworkit" php -- --install-dir=/usr/local/bin --filename=composer
