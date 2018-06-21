c0915fb722afb9af8fbb2ba833705cb5ddeb4339
Abdelrahman Omran <me@omranic.com>
2018-06-22 00:25:32 +0200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -5,7 +5,7 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 
 # Software Versions
-php = 7.2
+PHP = 7.2
 
 # Update Package List
 apt-get update
@@ -39,23 +39,23 @@ apt-get update
 apt-get install --no-install-recommends --no-install-suggests -y make curl \
 locales build-essential libpcre3-dev python2.7-dev ntp python-pip whois gcc supervisor nginx \
 sqlite3 nodejs wkhtmltopdf libmcrypt4 zip unzip git jpegoptim optipng pngquant gifsicle \
-php${php}-cli php${php}-dev php${php}-pgsql php${php}-sqlite3 php${php}-gd php${php}-fpm php${php}-xml \
-php${php}-curl php${php}-memcached php${php}-imap php${php}-mysql php${php}-mbstring \
-php${php}-zip php${php}-bcmath php${php}-soap php${php}-intl php${php}-readline \
-php${php}-redis mysql-server redis-server unattended-upgrades \
+php${PHP}-cli php${PHP}-dev php${PHP}-pgsql php${PHP}-sqlite3 php${PHP}-gd php${PHP}-fpm php${PHP}-xml \
+php${PHP}-curl php${PHP}-memcached php${PHP}-imap php${PHP}-mysql php${PHP}-mbstring \
+php${PHP}-zip php${PHP}-bcmath php${PHP}-soap php${PHP}-intl php${PHP}-readline \
+php${PHP}-redis mysql-server redis-server unattended-upgrades \
 mosquitto mosquitto-clients libmosquitto-dev
 
 # Install PHP-Mosquitto
 pecl install Mosquitto-alpha
 
-cat > /etc/php/${php}/mods-available/mosquitto.ini << EOF
+cat > /etc/php/${PHP}/mods-available/mosquitto.ini << EOF
 ; configuration for php common module
 ; priority=10
 extension=mosquitto.so
 EOF
 
-ln -s /etc/php/${php}/mods-available/mosquitto.ini /etc/php/${php}/cli/conf.d/20-mosquitto.ini
-ln -s /etc/php/${php}/mods-available/mosquitto.ini /etc/php/${php}/fpm/conf.d/20-mosquitto.ini
+ln -s /etc/php/${PHP}/mods-available/mosquitto.ini /etc/php/${PHP}/cli/conf.d/20-mosquitto.ini
+ln -s /etc/php/${PHP}/mods-available/mosquitto.ini /etc/php/${PHP}/fpm/conf.d/20-mosquitto.ini
 
 # Install svgo npm package
 npm install -g svgo
@@ -79,23 +79,23 @@ rm -rvf /etc/nginx/sites-enabled/default
 rm -rvf /etc/nginx/sites-available/default
 
 # Set Some PHP CLI Settings
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/${php}/cli/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php/${php}/cli/php.ini
-sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/${php}/cli/php.ini
-sed -i "s/;date.timezone = .*/date.timezone = UTC/" /etc/php/${php}/cli/php.ini
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/${PHP}/cli/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php/${PHP}/cli/php.ini
+sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/${PHP}/cli/php.ini
+sed -i "s/;date.timezone = .*/date.timezone = UTC/" /etc/php/${PHP}/cli/php.ini
 
 # Set Some PHP-FPM Options
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/${php}/fpm/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php/${php}/fpm/php.ini
-sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/${php}/fpm/php.ini
-sed -i "s/;date.timezone = .*/date.timezone = UTC/" /etc/php/${php}/fpm/php.ini
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/${PHP}/fpm/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php/${PHP}/fpm/php.ini
+sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/${PHP}/fpm/php.ini
+sed -i "s/;date.timezone = .*/date.timezone = UTC/" /etc/php/${PHP}/fpm/php.ini
 
 # Optimize OPcache for production
-sed -i "s/;opcache.enable=.*/opcache.enable=1/" /etc/php/${php}/fpm/php.ini
-sed -i "s/;opcache.memory_consumption=.*/opcache.memory_consumption=512/" /etc/php/${php}/fpm/php.ini
-sed -i "s/;opcache.max_accelerated_files=.*/opcache.max_accelerated_files=30000/" /etc/php/${php}/fpm/php.ini
-sed -i "s/;opcache.validate_timestamps=.*/opcache.validate_timestamps=0/" /etc/php/${php}/fpm/php.ini
-sed -i "s/;opcache.save_comments=.*/opcache.save_comments=1/" /etc/php/${php}/fpm/php.ini
+sed -i "s/;opcache.enable=.*/opcache.enable=1/" /etc/php/${PHP}/fpm/php.ini
+sed -i "s/;opcache.memory_consumption=.*/opcache.memory_consumption=512/" /etc/php/${PHP}/fpm/php.ini
+sed -i "s/;opcache.max_accelerated_files=.*/opcache.max_accelerated_files=30000/" /etc/php/${PHP}/fpm/php.ini
+sed -i "s/;opcache.validate_timestamps=.*/opcache.validate_timestamps=0/" /etc/php/${PHP}/fpm/php.ini
+sed -i "s/;opcache.save_comments=.*/opcache.save_comments=1/" /etc/php/${PHP}/fpm/php.ini
 
 # Replace default nginx config with optimized one
 rm -rvf /etc/nginx/nginx.conf
@@ -116,12 +116,12 @@ wget https://raw.githubusercontent.com/rinvex/cloudinit/develop/nginx/sites-avai
 ln -fs "/etc/nginx/sites-available/no-default.conf" "/etc/nginx/sites-enabled/no-default.conf"
 
 # Set The PHP-FPM User
-sed -i "s/user = www-data/user = rinvex/" /etc/php/${php}/fpm/pool.d/www.conf
-sed -i "s/group = www-data/group = rinvex/" /etc/php/${php}/fpm/pool.d/www.conf
+sed -i "s/user = www-data/user = rinvex/" /etc/php/${PHP}/fpm/pool.d/www.conf
+sed -i "s/group = www-data/group = rinvex/" /etc/php/${PHP}/fpm/pool.d/www.conf
 
-sed -i "s/listen\.owner.*/listen.owner = rinvex/" /etc/php/${php}/fpm/pool.d/www.conf
-sed -i "s/listen\.group.*/listen.group = rinvex/" /etc/php/${php}/fpm/pool.d/www.conf
-sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/${php}/fpm/pool.d/www.conf
+sed -i "s/listen\.owner.*/listen.owner = rinvex/" /etc/php/${PHP}/fpm/pool.d/www.conf
+sed -i "s/listen\.group.*/listen.group = rinvex/" /etc/php/${PHP}/fpm/pool.d/www.conf
+sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/${PHP}/fpm/pool.d/www.conf
 
 # Add User To WWW-Data
 usermod -a -G www-data rinvex
@@ -132,9 +132,9 @@ groups rinvex
 git clone https://github.com/Neilpang/acme.sh.git
 cd acme.sh && ./acme.sh --install  --accountemail  "a.omran@rinvex.com"
 
-# Restart nginx and php${php}-fpm services
+# Restart nginx and php${PHP}-fpm services
 /etc/init.d/nginx restart
-/etc/init.d/php${php}-fpm restart
+/etc/init.d/php${PHP}-fpm restart
 
 # Clean Up
 apt-get -y autoremove
